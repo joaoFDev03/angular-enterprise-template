d42315dc5aef8dd3f6bbe1eac8e029a19fec74c0
João Fernandes <joaopedrofer1509@gmail.com>
2026-01-02 18:54:56 +0000

    fixed __dirname error

--- scripts/rename-project.js
+++ scripts/rename-project.js
@@ -1,15 +1,18 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const newName = process.argv[2];
 if (!newName) throw new Error('Project name missing');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function replaceInFile(file) {
   const content = fs.readFileSync(file, 'utf8').replace(/angular-enterprise-template/g, newName);
   fs.writeFileSync(file, content);
 }
 
-// lista de ficheiros para substituir nomes
 ['package.json', 'README.md', 'angular.json'].forEach(f => replaceInFile(path.join(__dirname, '..', f)));
 
 console.log('Project renamed to', newName);
